16c82dbaa31dd32023cfb9c44d6b5d01a2f37353
Hasan Bayat <hasanbayat1393@gmail.com>
2018-12-13 11:11:07 +0330

    Fixed Connections Bug
    Fixed the bug relating to adding duplicate items to Connections of Node

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -9,37 +9,30 @@ using UnityEngine;
 public class Node : MonoBehaviour
 {
 
-	/// <summary>
-	/// The connections (neighbors).
-	/// </summary>
-	[SerializeField]
-	protected List<Node> m_Connections = new List<Node> ();
+    /// <summary>
+    /// The connections (neighbors).
+    /// </summary>
+    [SerializeField]
+    protected List<Node> m_Connections = new List<Node>();
 
-	/// <summary>
-	/// Gets the connections (neighbors).
-	/// </summary>
-	/// <value>The connections.</value>
-	public virtual List<Node> connections
-	{
-		get
-		{
-			return m_Connections;
-		}
-	}
+    /// <summary>
+    /// Gets the connections (neighbors).
+    /// </summary>
+    /// <value>The connections.</value>
+    public virtual List<Node> connections
+    {
+        get
+        {
+            return m_Connections;
+        }
+    }
 
-	public Node this [ int index ]
-	{
-		get
-		{
-			return m_Connections [ index ];
-		}
-	}
+    public Node this[int index]
+    {
+        get
+        {
+            return m_Connections[index];
+        }
+    }
 
-	void OnValidate ()
-	{
-		
-		// Removing duplicate elements
-		m_Connections = m_Connections.Distinct ().ToList ();
-	}
-	
 }
